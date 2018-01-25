eb5e359f6df653054bffb1a86ec275b4cf8162c8
Egor Grishechko <egorgrishechko@gmail.com>
2018-01-25 22:02:05 +0300

    rinkeby configuration

--- truffle.js
+++ truffle.js
@@ -4,6 +4,12 @@ module.exports = {
             host: "localhost",
             port: 8545,
             network_id: "*"
+        },
+        rinkeby: {
+            host: "localhost", // Connect to geth on the specified
+            port: 8545,
+            from: "0xC5ce18fF493F119cFd5fBDd8506Cf779d74678A2", // default address to use for any transaction Truffle makes during migrations
+            network_id: 4
         }
     }
 };
